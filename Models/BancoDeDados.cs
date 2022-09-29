4996774f9cb10a12aa4099162b955c1cd0f359e8
Hiago Alves <Hiago Alves@DESKTOP-CM8F2OC>
2022-09-28 21:18:21 -0300

    Adicionar arquivos de projeto.

---
+++ Models/BancoDeDados.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudCsharpv2.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=DESKTOP-CM8F2OC\SQLEXPRESS;Database=DWTestesCrud; Integrated Security=True");
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudCsharpv2.Models
+{
+    [Table("UsuarioTeste")]
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome{ get; set; }
+        public DateTime Idade{ get; set; }
+    }
+
+}
